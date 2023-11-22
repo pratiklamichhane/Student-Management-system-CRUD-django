7f100fe9f477b76a01fdcdd14aff1291378d2e56
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-11-22 17:36:14 +0545

    added a add student feature

--- base_project/students/urls.py
+++ base_project/students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('' , views.index , name='index'),
-    path('<int:id>' , views.view_student , name="view_student")
+    path('<int:id>' , views.view_student , name="view_student"),
+    path('add/' , views.add , name='add'),
 ]
--- base_project/students/views.py
+++ base_project/students/views.py
@@ -2,12 +2,45 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from .models import Student
+from .forms import studentForm
 # Create your views here.
 def index(request):
-    return render(request  ,'students/index.html', {
-        'students' : Student.objects.all()
+    query = request.GET.get('q')
+    students = Student.objects.all()
+    return render(request, 'students/index.html', {
+        'students': students,
     })
 
 def view_student(request , id):
     student = Student.objects.all( pk = id )
     return HttpResponseRedirect(reverse('index'))
+
+def add(request):
+    if request.method == "POST":
+        form = studentForm(request.POST)
+        if form.is_valid():
+            new_student_number = form.cleaned_data['student_number']
+            new_first_name = form.cleaned_data['first_name']
+            new_last_name = form.cleaned_data['last_name']
+            new_email = form.cleaned_data['email']
+            new_field_of_study = form.cleaned_data['field_of_study']
+            new_gpa = form.cleaned_data['gpa']
+            
+            new_student = Student(
+                student_number = new_student_number,
+                first_name = new_first_name,
+                last_name = new_last_name,
+                email = new_email,
+                field_of_study = new_field_of_study,
+                gpa = new_gpa,
+            )
+            new_student.save()
+            return render(request  , 'students/add.html',{
+                'form': studentForm(),
+                'success': True
+            })
+    else:
+        form = studentForm()
+    return render(request , 'students/add.html', {
+        'form': studentForm()
+    })
