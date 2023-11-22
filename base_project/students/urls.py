09671697966b4333950675283440c81ca800bd1f
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2023-11-22 10:48:13 +0545

    added student view option
    added view student option

--- base_project/students/urls.py
+++ base_project/students/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('' , views.index , name='index'),
+    path('<int:id>' , views.view_student , name="view_student")
 ]
--- base_project/students/views.py
+++ base_project/students/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from .models import Student
 # Create your views here.
@@ -5,3 +7,7 @@ def index(request):
     return render(request  ,'students/index.html', {
         'students' : Student.objects.all()
     })
+
+def view_student(request , id):
+    student = Student.objects.all( pk = id )
+    return HttpResponseRedirect(reverse('index'))
